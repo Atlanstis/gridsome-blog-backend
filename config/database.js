6f9d98bfd95cb7f9911bb3b69b19d5dd1a725839
Gypsophila <futur_@163.com>
2021-10-25 09:21:32 +0800

    初始化

---
+++ config/database.js
@@ -0,0 +1,17 @@
+module.exports = ({ env }) => ({
+  defaultConnection: "default",
+  connections: {
+    default: {
+      connector: "bookshelf",
+      settings: {
+        client: "mysql",
+        host: env("DATABASE_HOST", "113.31.125.222"),
+        port: env.int("DATABASE_PORT", 3306),
+        database: env("DATABASE_NAME", "blog"),
+        username: env("DATABASE_USERNAME", "root"),
+        password: env("DATABASE_PASSWORD", "8!DdZA%7rzgd6-$"),
+      },
+      options: {},
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '18fc9f6d-5f9c-4689-813c-f1044b1ce89e'
+};
